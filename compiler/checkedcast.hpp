8a0b9624378ddc907a2f74c24e4f75a034de4956
Ramon Asuncion <asuncionbatista@gmail.com>
2026-06-14 18:34:01 -0400

    Enable checked_cast assert

--- compiler/checkedcast.hpp
+++ compiler/checkedcast.hpp
@@ -4,8 +4,7 @@
 
 namespace ceramic {
 template <class To, class From> To checked_cast(From *from) {
-    // TODO: for unknown reason this code cause SEGV in runtime
-    // assert(!from || dynamic_cast<To>(from));
+    assert(!from || dynamic_cast<To>(from));
     return static_cast<To>(from);
 }
 } // namespace ceramic
